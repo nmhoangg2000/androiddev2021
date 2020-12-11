3e7e8eb57ca45457a7f6dc83f901fd2f9b3812f9
nmhoangg2000 <45127849+nmhoangg2000@users.noreply.github.com>
2020-12-11 11:53:58 +0700

    labwork

---
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -0,0 +1,58 @@
+package vn.edu.usth.weather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class WeatherActivity extends AppCompatActivity {
+    private static final String STATE = "Message";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+
+        Log.i(STATE,"onCreate");
+
+        ForecastFragment firstFragment = new ForecastFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.container, firstFragment).commit();
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+
+        Log.i(STATE,"onStart");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+
+        Log.i(STATE,"onResume");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+
+        Log.i(STATE,"onPause");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+
+        Log.i(STATE,"onStop");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+
+        Log.i(STATE,"onDestroy");
+    }
+
+
+}
